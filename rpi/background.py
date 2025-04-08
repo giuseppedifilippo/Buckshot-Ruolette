0894fe47e2de46bd84d6fcc12bdd170a0bd957bb
giuseppedifilippo <giuseppe.difilippo@live.it>
2025-04-08 17:10:57 +0200

    update

--- rpi/background.py
+++ rpi/background.py
@@ -3,7 +3,7 @@
 from playsound3 import playsound
 from pathlib import Path
 from sys import platform
-
+import pygame
 if platform == "win32" :
     win_path = r"C:\Users\giuseppe\Desktop\Buckshot-Ruolette\rpi\sounds\General_Release.mp3"
     universal_path = Path(win_path)
@@ -12,3 +12,4 @@ elif platform == "linux" :
     universal_path = Path(linux_path)
 #aggiungere l altro elif nel caso sia linux
 playsound(universal_path)
+
--- rpi/main.py
+++ rpi/main.py
@@ -1,10 +1,9 @@
 from asyncio import timeout
 from unittest import case
 import random, DCLL
-import
-#mappa che tiene tutte le variabili sui giocatori
-players = {1: {}, 2: {}, 3: {}, 4: {}}
-stats = {"lives": 0, "status": True}
+from playsound3 import playsound
+
+
 
 forbidden = [2, 3, 7, 15, 31, 61, 127]
 random.seed()
@@ -17,18 +16,33 @@ def new_mag():
         return new_mag()
     else:
         return bin(base)
+    #manda sequenza al sistema per mostrare le cartucce
 
-
-def subtract(player):
-    players[player]["lives"] -= 1
-    if players[player]["lives"] == 0:
-        stats["status"] = False
+def subtract(player, map):
+    map[player]["lives"] -= 1
+    if map[player]["lives"] == 0:
+        map[player]["status"] = False
         #send zapping sequence
 
 #genera la sequenza di giocatori
-el = None
-for i in range(int(input("Numero giocatori[2-4] : "))):
-    el = DCLL.insertAtBeginning(el, i)
-    el = el.next
+def startup() :
+    el = None
+    players = {1: {"lives": 3, "status": True}}
+    for i in range(1, int(input("Numero giocatori[2-4] : "))+1 ):
+        el = DCLL.insertAtBeginning(el, i)
+        el = el.next
+        if i == 1 :
+            continue
+        else :
+            players[i] = {"lives": 3, "status": True}
+    return el, players
 
-
+count = 0
+while True:
+    input("fuoco")
+    count = count +1
+    if count % 2 == 0 :
+        playsound(r"C:\Users\giuseppe\Desktop\Buckshot-Ruolette\rpi\sounds\audio_temp gunshot_live.wav", block=False)
+    else :
+        playsound(r"C:\Users\giuseppe\Desktop\Buckshot-Ruolette\rpi\sounds\audio_temp gunshot_blank.wav", block=False)
+    print(count)
