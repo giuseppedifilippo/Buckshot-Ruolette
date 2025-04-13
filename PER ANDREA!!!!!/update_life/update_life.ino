d34d3bfeb385e29d4e822cafee480f8ef8421b22
AndreaBenassi03 <a.benassi2003@gmail.com>
2025-04-13 16:30:58 +0200

    Aggiornata update_life per togliere e aggiungere più vite a giocatori diversi

--- PER ANDREA!!!!!/update_life/update_life.ino
+++ PER ANDREA!!!!!/update_life/update_life.ino
@@ -6,33 +6,41 @@
 int life=8;
 CRGB leds[n_led];
 String action="";
-
-void setup() {
-  FastLED.addLeds<WS2812B, data_pin, RGB>(leds, n_led);  
-  Serial.begin(9600);
-  clearLed();
-}
-
-void loop() {
-  checkSerial();
-  update_life();
-}
+String ins[3];
+//lifes and player indicated by instruction
+int lifes_ins = 0;
+int player = 0;
 
 void checkSerial(){
-  if(Serial.available()){
-    action = Serial.readString();
-    action.trim(); //to remove all the spaces
-    if(life>0){
-      if (action == "rm"){ //rm command to remove life
-        life -= 1;
-      }else if(action == "add" && life < n_led){ //add command to add life
-        life ++;
-      }
+  if (Serial.available()) {
+    action = Serial.readStringUntil('\n');
+    action.trim();
+
+    // divide the instruction in 3 parts
+    for (int i = 0; i < 3; i++) {
+      int index = action.indexOf(' ');
+      ins[i] = action.substring(0, index);
+      action = action.substring(index + 1);
     }
   }
 }
 
 void update_life(){
+  lifes_ins = ins[1].toInt();
+  player = ins[2].toInt(); // utile se hai un array di vite
+
+  String cmd = ins[0];
+
+  if (cmd == "rm") {
+    life -= lifes_ins;
+  } 
+  else if (cmd == "add" && (life + lifes_ins) <= n_led) {
+    life += lifes_ins;
+  }
+  action = "";
+}
+
+void update_led(){
   for(int i = 0; i<n_led; i++){
     if(i<life){
       leds[i] = CRGB::Green;
@@ -46,5 +54,19 @@ void update_life(){
 void clearLed(){
   for(int i=0; i<n_led; i++){
     leds[i]=CRGB::Black;
+  }
+}
+
+void setup() {
+  FastLED.addLeds<WS2812B, data_pin, RGB>(leds, n_led);  
+  Serial.begin(9600);
+  clearLed();
+}
+
+void loop() {
+  checkSerial();
+  update_led();
+  if(action != ""){
+    update_life();
   }
 }
