f733a23175699b02ed0a3aedf9b9c68d53a46b8e
AndreaBenassi03 <a.benassi2003@gmail.com>
2025-04-15 10:13:40 +0200

    Creato il file che muove servo SG90 0, 120 e 240 gradi

---
+++ PER ANDREA!!!!!/servo_move.ino/servo_move/servo_move.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos1 = 0;
+int pos2 = 120;
+int pos3 = 240; 
+int pinServo = 3;
+
+void setup() {
+  myservo.attach(pinServo);
+}
+
+void loop() {
+  myservo.write(pos1);
+  delay(100);
+  myservo.write(pos2);
+  delay(100);
+  myservo.write(pos3);
+  delay(100);
+}
