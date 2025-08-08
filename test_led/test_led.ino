0d6c9cf9b30b151b84fca990587d66ae5d28f2a7
AndreaBenassi03 <a.benassi2003@gmail.com>
2025-08-08 20:55:54 +0200

    Creato test leds

---
+++ test_led/test_led.ino
@@ -0,0 +1,23 @@
+#include <FastLED.h>
+
+#define data_pin 3
+#define n_led 12
+
+CRGB led[12];
+
+void setup(){
+  FastLED.addLeds<WS2812B, data_pin, RGB>(led, n_led);
+}
+
+void loop(){
+  for(int i=0; i<6; i++){
+    led[i]=CRGB::Green;
+    led[i+1]=CRGB::Green;
+    FastLED.show();
+    delay(1000);
+  }
+  for(int i=0; i<12; i++){
+    led[i]=CRGB::Black;
+  }
+  FastLED.show();
+}
